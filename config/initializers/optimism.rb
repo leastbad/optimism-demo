6672d5f80e70a0e1a94324f808bacd42780404e9
leastbad <hello@leastbad.com>
2020-12-19 11:14:53 -0500

    v0.4.0

--- config/initializers/optimism.rb
+++ config/initializers/optimism.rb
@@ -1,3 +1,3 @@
 Optimism.configure do |config|
-  config.channel = ->(context) { OptimismChannel.broadcasting_for(context.session.id) }
+  config.channel_proc = ->(context) { OptimismChannel.broadcasting_for(context.session.id) }
 end
